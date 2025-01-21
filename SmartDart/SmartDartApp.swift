af5a873586ea4efdb02601088fe08bee34b834e9
Nate Schaefer <nathanschaefer14@gmail.com>
2025-01-21 15:55:14 -0600

    Initial Commit

---
+++ SmartDart/SmartDartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartDartApp.swift
+//  SmartDart
+//
+//  Created by Nathan Schaefer on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartDartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartDartTests/SmartDartTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartDartTests.swift
+//  SmartDartTests
+//
+//  Created by Nathan Schaefer on 1/21/25.
+//
+
+import Testing
+@testable import SmartDart
+
+struct SmartDartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
